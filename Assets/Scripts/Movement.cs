be0028a4a07edaa5b0a2b60c0c6e3c502644e2ea
Sidney Nimako <snibo13@gmail.com>
2022-09-03 19:08:04 -0400

    Initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Vector3 movement;
+
+    public float movement_speed = 0.005f;
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        gameObject.GetComponent<Animator>().SetFloat("x", movement.x);
+        gameObject.GetComponent<Animator>().SetFloat("y", movement.y);
+        gameObject.GetComponent<Animator>().SetFloat("speed", movement.sqrMagnitude);
+
+    }
+
+    void FixedUpdate()
+    {
+
+        gameObject.transform.position += movement_speed * movement * Time.deltaTime;
+        
+    }
+
+}
+
